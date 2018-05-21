63f08ce505cc295232f27e386848076c0b225cb1
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-21 11:43:23 -0400

    scraper working

--- yahoo-finance-scraper.js
+++ yahoo-finance-scraper.js
@@ -10,12 +10,15 @@ function getStockInfo(ticker, callback) {
     var $ = cheerio.load(res.body, {decodeEntities: false});
     stocks.lastPrice = $('[data-reactid="34"]').find('[data-reactid="35"]').first().html();
     stocks.yestPrice = $("table").find("[data-reactid='41']").text();
-    stocks.change = (parseFloat(stocks.lastPrice.replace(",", "")) - parseFloat(stocks.yestPrice.replace(",", ""))).toFixed(2);
+    let lastPrice = parseFloat(stocks.lastPrice.replace(",", "")).toFixed(2);
+    let yestPrice = parseFloat(stocks.yestPrice.replace(",", "")).toFixed(2);
+    stocks.change = (lastPrice - yestPrice).toFixed(2);
+    stocks.changePct = (((yestPrice - lastPrice) / lastPrice) * 100).toFixed(2);
     stocks.marketCap = $("table").find('[data-reactid="82"]').html();
     stocks.vol = $("table").find('[data-reactid="69"]').html();
     stocks.avgVol = $("table").find('[data-reactid="74"]').html();
     stocks.marketTime = $('#quote-market-notice').find('[data-reactid="38"]').text().substr(7,11);
-    stocks.currency = $('#quote-header-info').find('[data-reactid="5"]').find('[data-reactid="8"]').find('[data-reactid="9"]').text().substr(49);
+    stocks.currency = $('#quote-header-info').find('[data-reactid="5"]').find('[data-reactid="8"]').find('[data-reactid="9"]').text().substr(-3);
     callback(stocks);
   });
 }
